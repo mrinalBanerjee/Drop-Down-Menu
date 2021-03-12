8a86a26959138d2d64bd196e4938856a28499bdb
Mrinal banerjee <61612873+mrinalBanerjee@users.noreply.github.com>
2021-03-12 12:41:16 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+
+    var button = document.querySelector(".button");
+
+    button.addEventListener("click",show);
+
+    function show(){
+
+    var list = document.querySelector(".dropdown");
+
+    list.classList.toggle("active");
+
+    }
+
+  
